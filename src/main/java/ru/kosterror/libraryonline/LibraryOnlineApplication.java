2c363b07cc4453cb8e0e2bcad867c1a118794862
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-04-21 18:02:50 +0700

    Initial commit

---
+++ src/main/java/ru/kosterror/libraryonline/LibraryOnlineApplication.java
@@ -0,0 +1,13 @@
+package ru.kosterror.libraryonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryOnlineApplication.class, args);
+	}
+
+}
